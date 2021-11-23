60a47e183cf887ef806ac996f476b110749c0e23
Alexander Ten <orionapplepie@gmail.com>
2021-11-23 22:20:11 +1000

    Detect eyes and smiles only on face.

--- config.py
+++ config.py
@@ -1,23 +1,29 @@
-CASCADES = {
-    'Face front': {
-        'path': 'haarcascades/faces/haarcascade_frontalface_default.xml',
-        'color': (255, 0, 0),
-        'draw': True
-    },
-    'Face profile': {
-        'path': 'haarcascades/faces/haarcascade_profileface.xml',
-        'color': (255, 0, 0),
-        'draw': True
-    },
+
+SUB_FACE_CASCADES = {
     'Smile': {
         'path': 'haarcascades/faces/haarcascade_smile.xml',
         'color': (0, 255, 0),
-        'draw': True
+        'draw': True,
     },
     'Eyes': {
         'path': 'haarcascades/faces/haarcascade_eye.xml',
         'color': (0, 0, 255),
-        'draw': True
+        'draw': True,
+    },
+}
+
+CASCADES = {
+    'Face front': {
+        'path': 'haarcascades/faces/haarcascade_frontalface_default.xml',
+        'color': (255, 0, 0),
+        'draw': True,
+        'sub_cascades': SUB_FACE_CASCADES,
+    },
+    'Face profile': {
+        'path': 'haarcascades/faces/haarcascade_profileface.xml',
+        'color': (255, 0, 0),
+        'draw': True,
+        'sub_cascades': SUB_FACE_CASCADES,
     },
     'Full body': {
         'path': 'haarcascades/faces/haarcascade_fullbody.xml',
--- run.py
+++ run.py
@@ -19,35 +19,61 @@ def draw_rect(frame, x, y, w, h, color):
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
 
-def get_cascades():
+def make_cascades(cascades=None):
+    """Collect cv2 cascade objects from config with nested cascads."""
+    if cascades is None:
+        return None
     return [
-        (cv2.CascadeClassifier(cascade['path']), cascade['color'])
-        for _, cascade in CASCADES.items()
+        {
+            'cascade': cv2.CascadeClassifier(cascade['path']),
+            'color': cascade['color'],
+            'sub_cascades': make_cascades(cascade.get('sub_cascades', None))
+        }
+        for _, cascade in cascades.items()
         if cascade['draw']
     ]
 
 
+def detect(cascades=None, gray_frame=None, cam_frame=None):
+    """Detect objects in frame."""
+
+    for cascade in cascades:
+        color = cascade['color']
+        captures = cascade['cascade'].detectMultiScale(
+            gray_frame,
+            scaleFactor=1.1,
+            minNeighbors=10,
+            minSize=(30, 30)
+        )
+        sub_cascades = cascade['sub_cascades']
+        for (x, y, w, h) in captures:
+            draw_rect(cam_frame, x, y, w, h, color)
+
+            if sub_cascades is not None:
+                detect(
+                    sub_cascades,
+                    gray_frame[y:y + h, x:x + w],
+                    cam_frame[y:y + h, x:x + w]
+                )
+
+
 def main():
-    cascades = get_cascades()
+    cascades = make_cascades(CASCADES)
     video_capture = cv2.VideoCapture(0)
 
     while True:
         if not video_capture.isOpened():
             print("Couldn't find your webcam... Sorry :c")
             sys.exit(0)
-        _, webcam_frame = video_capture.read()
-        gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
 
-        for cascade, color in cascades:
-            captures = cascade.detectMultiScale(
-                    gray_frame,
-                    scaleFactor=1.1,
-                    minNeighbors=10,
-                    minSize=(30, 30)
-                )
-            for (x, y, w, h) in captures:
-                draw_rect(webcam_frame, x, y, w, h, color)
-        show_frame(webcam_frame)
+        _, cam_frame = video_capture.read()
+        gray_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2GRAY)
+        detect(
+            cascades=cascades,
+            gray_frame=gray_frame,
+            cam_frame=cam_frame
+        )
+        show_frame(cam_frame)
 
         if is_user_wants_quit():
             break
